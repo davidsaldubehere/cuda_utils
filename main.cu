87ef99e841d0e4b0dcf968a673814d89311912ee
davesaldub <davesaldub@gmail.com>
2024-04-07 14:41:07 -0400

    added matrix mul testing

--- main.cu
+++ main.cu
@@ -1,12 +1,13 @@
 #include "./common_ops/vector_add.h"
 #include "./common_ops/matrix_mul.h"
+#include <cassert>
 #include <iostream>
 #include <vector>
 
 int main() {
 
     /*Vector addition*/
-    const int N = 1 << 16;  // Array size of 2^16 (65536 elements)
+    int N = 1 << 16;  // Array size of 2^16 (65536 elements)
     std::vector<int> a(N);
     std::vector<int> b(N);
     std::vector<int> result(N);
@@ -30,16 +31,30 @@ int main() {
 
     std::cout << "COMPLETED SUCCESSFULLY!" << std::endl;
     
-    int N = 1 << 10; // 1024x1024 matrix
-    std::vector<int> a(N * N);
-    std::vector<int> b(N * N);
-    std::vector<int> c(N * N);
+    N = 1 << 10; // 1024x1024 matrix
+    std::vector<int> d(N * N);
+    std::vector<int> e(N * N);
+    std::vector<int> f(N * N);
 
     // Initialize matrices...
 
-    matrix_mul(a, b, c, N);
+    matrix_mul(d, e, f, N);
 
     // Verify result...
+    
+    for (int i = 0; i < N; i++) {
+    // For every column...
+        for (int j = 0; j < N; j++) {
+        // For every element in the row-column pair
+        int tmp = 0;
+        for (int k = 0; k < N; k++) {
+            // Accumulate the partial results
+            tmp += d[i * N + k] * e[k * N + j];
+        }
 
+        // Check against the CPU result
+        assert(tmp == f[i * N + j]); //takes a while
+        }
+  }
     return 0;
 }
